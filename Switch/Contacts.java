e297c788156615cd7810c72ec2d6073e85010a4c
ruchithav29 <ruchithavs210@gmail.com>
2026-03-16 09:41:03 +0530

    Switch

---
+++ Switch/Contacts.java
@@ -0,0 +1,772 @@
+ class Contacts
+{
+	static long searchContactsByName(String ContactsName){
+	
+	long number = 0;
+	switch(ContactsName){
+
+	case "Appa":
+		System.out.println("case 1");
+		number = 9754628920L;
+		break;
+
+	case "amma":
+		System.out.println("case 2");
+		number = 9489768735L;
+		break;
+
+	case "rohi":
+		System.out.println("case 3");
+		number = 7909898058L;
+		break;
+
+	case "kaveri":
+		System.out.println("case 4");
+		number = 9745654788L;
+		break;
+
+	case "cutie":
+		System.out.println("case 5");
+		number = 9734542532L;
+		break;
+
+	case "manasa":
+		System.out.println("case 6");
+		number = 9734542576L;
+		break;
+
+	case "abhijna":
+		System.out.println("case 7");
+		number = 9489767654L;
+		break;
+
+	case "abhishekmca":
+		System.out.println("case 8");
+		number = 7909879087L;
+		break;
+
+	case "adhithisis":
+		System.out.println("case 9");
+		number = 9745676545L;
+		break;
+
+	case "aishwarya":
+		System.out.println("case 10");
+		number = 9734542598L;
+		break;
+
+	case "aishwaryatn":
+		System.out.println("case 11");
+		number = 9734542509L;
+		break;
+
+	case "akka":
+		System.out.println("case 12");
+		number = 9489767612L;
+		break;
+
+	case "akshatha":
+		System.out.println("case 13");
+		number = 7909879013L;
+		break;
+
+	case "amma1":
+		System.out.println("case 14");
+		number = 9745676514L;
+		break;
+
+	case "amulya":
+		System.out.println("case 15");
+		number = 9734542515L;
+		break;
+
+	case "anandh":
+		System.out.println("case 16");
+		number = 9734542516L;
+		break;
+
+	case "anjali":
+		System.out.println("case 17");
+		number = 9489767617L;
+		break;
+
+	case "anjalicgckm":
+		System.out.println("case 18");
+		number = 7909879018L;
+		break;
+
+	case "ankal":
+		System.out.println("case 19");
+		number = 9745676519L;
+		break;
+
+	case "ankitha":
+		System.out.println("case 20");
+		number = 9734542523L;
+		break;
+
+	case "anuantie":
+		System.out.println("case 21");
+		number = 9734542565L;
+		break;
+
+	case "anuhya":
+		System.out.println("case 22");
+		number = 9489767987L;
+		break;
+
+	case "anvitha":
+		System.out.println("case 23");
+		number = 7909879098L;
+		break;
+
+	case "archana":
+		System.out.println("case 24");
+		number = 9745676763L;
+		break;
+
+	case "arpitha":
+		System.out.println("case 25");
+		number = 9734542096L;
+		break;
+		
+	case "arunanna":
+		System.out.println("case 26");
+		number = 9734542874L;
+	break;
+
+	case "arunsir":
+		System.out.println("case 27");
+		number = 9489767956L;
+		break;
+
+	case "ashamca":
+		System.out.println("case 28");
+		number = 9489767998L;
+		break;
+
+	case "asharaniakka":
+		System.out.println("case 29");
+		number = 7909879097L;
+		break;
+
+	case "ashitha":
+		System.out.println("case 30");
+		number = 9745676796L;
+		break;
+
+	case "ashwinimakeover":
+		System.out.println("case 31");
+		number = 9734542096L;
+		break;
+
+	case "ashwiniscl":
+		System.out.println("case 32");
+		number = 9734542882L;
+		break;
+
+	case "ashwinisis":
+		System.out.println("case 33");
+		number = 7909877856L;
+		break;
+
+	case "ashwinitmp":
+		System.out.println("case 34");
+		number = 9745677834L;
+		break;
+
+	case "athmiya":
+		System.out.println("case 35");
+		number = 9734547890L;
+		break;
+
+	case "babyantie":
+		System.out.println("case 36");
+		number = 9734547802L;
+		break;
+
+	case "bindhu":
+		System.out.println("case 37");
+		number = 9489760945L;
+		break;
+
+	case "bindhutrailer":
+		System.out.println("case 38");
+		number = 7909890962L;
+		break;
+
+	case "brundha":
+		System.out.println("case 39");
+		number = 9745650921L;
+		break;
+
+	case "candrikackm":
+		System.out.println("case 40");
+		number = 9734540987L;
+		break;
+
+	case "chaithra":
+		System.out.println("case 41");
+		number = 9734540932L;
+		break;
+
+	case "chaithrabca":
+		System.out.println("case 42");
+		number = 9489760945L;
+		break;
+
+	case "chandanasenior":
+		System.out.println("case 43");
+		number = 7909890962L;
+		break;
+
+	case "chandanasgsis":
+		System.out.println("case 44");
+		number = 9745650921L;
+		break;
+
+	case "chandhana":
+		System.out.println("case 45");
+		number = 9734540987L;
+		break;
+
+	case "chandhanackm":
+		System.out.println("case 46");
+		number = 9734540932L;
+		break;
+
+	case "chandhanak":
+		System.out.println("case 47");
+		number = 9489760908L;
+		break;
+
+	case "chandhuk":
+		System.out.println("case 48");
+		number = 7909870943L;
+		break;
+
+	case "chandhup":
+		System.out.println("case 49");
+		number = 9745670921L;
+		break;
+
+	case "chandra":
+		System.out.println("case 50");
+		number = 9734540927L;
+		break;
+
+	case "chandrika":
+		System.out.println("case 51");
+		number = 9734540976L;
+		break;
+
+	case "chandumca":
+		System.out.println("case 52");
+		number = 9489767654L;
+		break;
+
+	case "charu":
+		System.out.println("case 53");
+		number = 7909879087L;
+		break;
+
+	case "chathurthibca":
+		System.out.println("case 54");
+		number = 9745676545L;
+		break;
+
+	case "danalakshmi":
+		System.out.println("case 55");
+		number = 9734542598L;
+		break;
+
+	case "deekshithmca":
+		System.out.println("case 56");
+		number = 9734542509L;
+		break;
+
+	case "deepaksir":
+		System.out.println("case 57");
+		number = 9489767654L;
+		break;
+
+	case "deepika":
+		System.out.println("case 58");
+		number = 7909879087L;
+		break;
+
+	case "dekshitha":
+		System.out.println("case 59");
+		number = 9745676545L;
+		break;
+
+	case "dhanu":
+		System.out.println("case 60");
+		number = 9734542598L;
+		break;
+
+	case "dhanya":
+		System.out.println("case 61");
+		number = 9734542509L;
+		break;
+
+	case "dhodamma":
+		System.out.println("case 62");
+		number = 9489767987L;
+		break;
+
+	case "dhodappa":
+		System.out.println("case 63");
+		number = 7909879098L;
+		break;
+
+	case "dhumalli":
+		System.out.println("case 64");
+		number = 9745676763L;
+		break;
+
+	case "dineshmava":
+		System.out.println("case 65");
+		number = 9734542096L;
+		break;
+
+	case "directormu":
+		System.out.println("case 66");
+		number = 9734542874L;
+		break;
+
+	case "divyabujji":
+		System.out.println("case 67");
+		number = 9489767956L;
+		break;
+
+	case "drrohi":
+		System.out.println("case 68");
+		number = 9489767998L;
+		break;
+
+	case "dvsxerox":
+		System.out.println("case 69");
+		number = 7909879097L;
+		break;
+
+	case "faizu":
+		System.out.println("case 70");
+		number = 9745676796L;
+		break;
+
+	case "fansii":
+		System.out.println("case 71");
+		number = 9734542096L;
+		break;
+
+	case "gagana":
+		System.out.println("case 72");
+		number = 9489767987L;
+		break;
+
+	case "ganashree":
+		System.out.println("case 73");
+		number = 7909879098L;
+		break;
+
+	case "ganeshjewellery":
+		System.out.println("case 74");
+		number = 9745676763L;
+		break;
+
+	case "ganeshvinbnagar":
+		System.out.println("case 75");
+		number = 9734542096L;
+		break;
+
+	case "geetha":
+		System.out.println("case 76");
+		number = 9734542874L;
+		break;
+
+	case "geetha1":
+		System.out.println("case 77");
+		number = 7909879097L;
+		break;
+
+	case "girishbatru":
+		System.out.println("case 78");
+		number = 9489767998L;
+		break;
+
+	case "gourimca":
+		System.out.println("case 79");
+		number = 7909879097L;
+		break;
+
+	case "gowthammca":
+		System.out.println("case 80");
+		number = 9745676796L;
+		break;
+	
+	case "gvm":
+	System.out.println("case 81");
+	number = 9734542096L;
+	break;
+
+	case "haleshsir":
+		System.out.println("case 82");
+		number = 9731542096L;
+		break;
+
+	case "haleshd":
+		System.out.println("case 83");
+		number = 9732542882L;
+		break;
+
+	case "harish":
+		System.out.println("case 84");
+		number = 7903877856L;
+		break;
+
+	case "harshini":
+		System.out.println("case 85");
+		number = 9744677834L;
+		break;
+
+	case "harshinibca":
+		System.out.println("case 86");
+		number = 9735547890L;
+		break;
+
+	case "harshitha":
+		System.out.println("case 87");
+		number = 9736547802L;
+		break;
+
+	case "heeba":
+		System.out.println("case 88");
+		number = 9487760945L;
+		break;
+
+	case "incharamca":
+		System.out.println("case 89");
+		number = 7909890962L;
+		break;
+
+	case "indhuja":
+		System.out.println("case 90");
+		number = 9748650921L;
+		break;
+
+	case "isiripg":
+		System.out.println("case 91");
+		number = 9730540987L;
+		break;
+
+	case "jaggaankal":
+		System.out.println("case 92");
+		number = 9732540932L;
+		break;
+
+	case "janyashree":
+		System.out.println("case 93");
+		number = 7909821098L;
+		break;
+
+	case "jnanavi":
+		System.out.println("case 94");
+		number = 9745623763L;
+		break;
+
+	case "joythika":
+		System.out.println("case 95");
+		number = 9734524096L;
+		break;
+
+	case "joythi1":
+		System.out.println("case 96");
+		number = 9734524096L;
+		break;
+
+	case "kalilsir":
+		System.out.println("case 97");
+		number = 9489726956L;
+		break;
+
+	case "kalyaniankal":
+		System.out.println("case 98");
+		number = 9489727998L;
+		break;
+
+	case "kamalaauntie":
+		System.out.println("case 99");
+		number = 7909828097L;
+		break;
+
+	case "kamala1":
+		System.out.println("case 100");
+		number = 7909828097L;
+		break;
+
+	case "kanakashree":
+		System.out.println("case 101");
+		number = 9734531096L;
+		break;
+
+	case "kavanaakka":
+		System.out.println("case 102");
+		number = 9489722987L;
+		break;
+
+	case "kavanasharath":
+		System.out.println("case 103");
+		number = 7909854098L;
+		break;
+
+	case "kavanaskg":
+		System.out.println("case 104");
+		number = 9745653763L;
+		break;
+
+	case "kavya":
+		System.out.println("case 105");
+		number = 9734572096L;
+		break;
+
+	case "kavyaakkabnglr":
+		System.out.println("case 106");
+		number = 9734571874L;
+		break;
+
+	case "kavyaanjali":
+		System.out.println("case 107");
+		number = 9489773956L;
+		break;
+
+	case "kavyabarki":
+		System.out.println("case 108");
+		number = 9489778998L;
+		break;
+
+	case "keerthana":
+		System.out.println("case 109");
+		number = 7909877097L;
+		break;
+
+	case "keerthanahyd":
+		System.out.println("case 110");
+		number = 9745634796L;
+		break;
+
+	case "keerthi":
+		System.out.println("case 111");
+		number = 9734533096L;
+		break;
+
+	case "krish":
+		System.out.println("case 112");
+		number = 9734542096L;
+		break;
+
+	case "kullamava":
+		System.out.println("case 113");
+		number = 9489767987L;
+		break;
+
+	case "lakshmitailor":
+		System.out.println("case 114");
+		number = 7909879098L;
+		break;
+
+	case "lathaantie":
+		System.out.println("case 115");
+		number = 9745676763L;
+		break;
+
+	case "lavanya":
+		System.out.println("case 116");
+		number = 9734542096L;
+		break;
+
+	case "leelavegetable":
+		System.out.println("case 117");
+		number = 9734542874L;
+		break;
+
+	case "leelavathi":
+		System.out.println("case 118");
+		number = 9489767956L;
+		break;
+
+	case "likitha":
+		System.out.println("case 119");
+		number = 9489767998L;
+		break;
+
+	case "lohithanna":
+		System.out.println("case 120");
+		number = 7909879097L;
+		break;
+
+	case "lokeshmava":
+		System.out.println("case 121");
+		number = 9745676796L;
+		break;
+
+	case "malikadhodamma":
+		System.out.println("case 122");
+		number = 9734542096L;
+		break;
+
+	case "manjulaantie":
+		System.out.println("case 123");
+		number = 9731542096L;
+		break;
+
+	case "manjunathasir":
+		System.out.println("case 124");
+		number = 9732542882L;
+		break;
+
+	case "manu":
+		System.out.println("case 125");
+		number = 7903877856L;
+		break;
+
+	case "manubro":
+		System.out.println("case 126");
+		number = 9744677834L;
+		break;
+
+	case "medhu":
+		System.out.println("case 127");
+		number = 9735547890L;
+		break;
+
+	case "modelingsmg":
+		System.out.println("case 128");
+		number = 9736547802L;
+		break;
+
+	case "modell":
+		System.out.println("case 129");
+		number = 9487760945L;
+		break;
+
+	case "modell1":
+		System.out.println("case 130");
+		number = 7909890962L;
+		break;
+
+	case "modell2":
+		System.out.println("case 131");
+		number = 9748650921L;
+		break;
+
+	case "monisha":
+		System.out.println("case 132");
+		number = 9730540987L;
+		break;
+
+	case "naliniakka":
+		System.out.println("case 133");
+		number = 7909890962L;
+		break;
+
+	case "namanackm":
+		System.out.println("case 134");
+		number = 9745650921L;
+		break;
+
+	case "nandhinipg":
+		System.out.println("case 135");
+		number = 9734540987L;
+		break;
+
+	case "nanjundhaankal":
+		System.out.println("case 136");
+		number = 9734540932L;
+		break;
+
+	case "nayana":
+		System.out.println("case 137");
+		number = 9489760908L;
+		break;
+
+	case "nayanamca":
+		System.out.println("case 138");
+		number = 7909870943L;
+		break;
+
+	case "neha":
+		System.out.println("case 139");
+		number = 9745670921L;
+		break;
+
+	case "nehaakkimca":
+		System.out.println("case 140");
+		number = 9734540927L;
+		break;
+
+	case "nehabca":
+		System.out.println("case 141");
+		number = 9734540976L;
+		break;
+
+	case "naliniakka":
+		System.out.println("case 142");
+		number = 7909890962L;
+		break;
+
+	case "namanackm":
+		System.out.println("case 143");
+		number = 9745650921L;
+		break;
+
+	case "nandhinipg":
+		System.out.println("case 144");
+		number = 9734540987L;
+		break;
+
+	case "nanjundhaankal":
+		System.out.println("case 145");
+		number = 9734540932L;
+		break;
+
+	case "nayana":
+		System.out.println("case 146");
+		number = 9489760908L;
+		break;
+
+	case "nayanamca":
+		System.out.println("case 147");
+		number = 7909870943L;
+		break;
+
+	case "neha":
+		System.out.println("case 148");
+		number = 9745670921L;
+		break;
+
+	case "nehaakkimca":
+		System.out.println("case 149");
+		number = 9734540927L;
+		break;
+
+	case "nehabca":
+		System.out.println("case 150");
+		number = 9734540976L;
+		break;
+	
+	default:
+		System.out.println("no name match");
+	break;
+	
+	}
+		return number;
+	}
+}
+	
+	
+	
+	
+	
+	
+	
---
+++ Switch/ContactsRunner.java
@@ -0,0 +1,410 @@
+class ContactsRunner
+{
+	public static void main(String [] args)
+	{
+		System.out.println("Phone Numbers In My Contact List are...");
+	
+		long number1 = Contacts.searchContactsByName("Appa");
+		System.out.println(number1);
+		
+		long number2 = Contacts.searchContactsByName("Amma");
+		System.out.println(number2); 
+		
+		long number3 = Contacts.searchContactsByName("Rohi");
+		System.out.println(number3);
+		
+		long number4 = Contacts.searchContactsByName("cutie");
+		System.out.println(number4);
+		
+		long number5 = Contacts.searchContactsByName("manasa");
+		System.out.println(number5);
+		
+		long number6 = Contacts.searchContactsByName("abhijna");
+		System.out.println(number6);
+		
+		long number7 = Contacts.searchContactsByName("abhishekmca");
+		System.out.println(number7);
+		
+		long number8 = Contacts.searchContactsByName("adhithisis");
+		System.out.println(number8);
+		
+		long number9 = Contacts.searchContactsByName("aishwarya");
+		System.out.println(number9);
+		
+		long number10 = Contacts.searchContactsByName("aishwaryatn");
+		System.out.println(number10);
+		
+		long number11 = Contacts.searchContactsByName("anuhya");
+		System.out.println(number11);
+
+		long number12 = Contacts.searchContactsByName("anvitha");
+		System.out.println(number12);
+
+		long number13 = Contacts.searchContactsByName("archana");
+		System.out.println(number13);
+
+		long number14 = Contacts.searchContactsByName("arpitha");
+		System.out.println(number14);
+
+		long number15 = Contacts.searchContactsByName("arunanna");
+		System.out.println(number15);
+
+		long number16 = Contacts.searchContactsByName("arunsir");
+		System.out.println(number16);
+
+		long number17 = Contacts.searchContactsByName("ashamca");
+		System.out.println(number17);
+
+		long number18 = Contacts.searchContactsByName("asharaniakka");
+		System.out.println(number18);
+
+		long number19 = Contacts.searchContactsByName("ashitha");
+		System.out.println(number19);
+
+		long number20 = Contacts.searchContactsByName("ashwinimakeover");
+		System.out.println(number20);
+
+		long number21 = Contacts.searchContactsByName("ashwiniscl");
+		System.out.println(number21);
+
+		long number22 = Contacts.searchContactsByName("ashwinisis");
+		System.out.println(number22);
+
+		long number23 = Contacts.searchContactsByName("ashwinitmp");
+		System.out.println(number23);
+
+		long number24 = Contacts.searchContactsByName("athmiya");
+		System.out.println(number24);
+
+		long number25 = Contacts.searchContactsByName("babyantie");
+		System.out.println(number25);
+
+		long number26 = Contacts.searchContactsByName("bindhu");
+		System.out.println(number26);
+
+		long number27 = Contacts.searchContactsByName("bindhutrailer");
+		System.out.println(number27);
+
+		long number28 = Contacts.searchContactsByName("brundha");
+		System.out.println(number28);
+
+		long number29 = Contacts.searchContactsByName("candrikackm");
+		System.out.println(number29);
+
+		long number30 = Contacts.searchContactsByName("chaithra");
+		System.out.println(number30);
+		
+		long number31 = Contacts.searchContactsByName("chaithrabca");
+		System.out.println(number31);
+
+		long number32 = Contacts.searchContactsByName("chandanasenior");
+		System.out.println(number32);
+
+		long number33 = Contacts.searchContactsByName("chandanasgsis");
+		System.out.println(number33);
+
+		long number34 = Contacts.searchContactsByName("chandhana");
+		System.out.println(number34);
+
+		long number35 = Contacts.searchContactsByName("chandhanackm");
+		System.out.println(number35);
+
+		long number36 = Contacts.searchContactsByName("chandhanak");
+		System.out.println(number36);
+
+		long number37 = Contacts.searchContactsByName("chandhup");
+		System.out.println(number37);
+
+		long number38 = Contacts.searchContactsByName("chandra");
+		System.out.println(number38);
+
+		long number39 = Contacts.searchContactsByName("chandrika");
+		System.out.println(number39);
+
+		long number40 = Contacts.searchContactsByName("chandumca");
+		System.out.println(number40);
+
+		long number41 = Contacts.searchContactsByName("charu");
+		System.out.println(number41);
+
+		long number42 = Contacts.searchContactsByName("chathurthibca");
+		System.out.println(number42);
+
+		long number43 = Contacts.searchContactsByName("danalakshmi");
+		System.out.println(number43);
+
+		long number44 = Contacts.searchContactsByName("deekshithmca");
+		System.out.println(number44);
+
+		long number45 = Contacts.searchContactsByName("deepaksir");
+		System.out.println(number45);
+
+		long number46 = Contacts.searchContactsByName("deepika");
+		System.out.println(number46);
+
+		long number47 = Contacts.searchContactsByName("dekshitha");
+		System.out.println(number47);
+
+		long number48 = Contacts.searchContactsByName("dhanu");
+		System.out.println(number48);
+
+		long number49 = Contacts.searchContactsByName("dhanya");
+		System.out.println(number49);
+
+		long number50 = Contacts.searchContactsByName("dhodamma");
+		System.out.println(number50);
+
+		long number51 = Contacts.searchContactsByName("dhodappa");
+		System.out.println(number51);
+
+		long number52 = Contacts.searchContactsByName("dhumalli");
+		System.out.println(number52);
+
+		long number53 = Contacts.searchContactsByName("dineshmava");
+		System.out.println(number53);
+
+		long number54 = Contacts.searchContactsByName("directormu");
+		System.out.println(number54);
+
+		long number55 = Contacts.searchContactsByName("divyabujji");
+		System.out.println(number55);
+
+		long number56 = Contacts.searchContactsByName("drrohi");
+		System.out.println(number56);
+
+		long number57 = Contacts.searchContactsByName("dvsxerox");
+		System.out.println(number57);
+
+		long number58 = Contacts.searchContactsByName("faizu");
+		System.out.println(number58);
+
+		long number59 = Contacts.searchContactsByName("fansii");
+		System.out.println(number59);
+
+		long number60 = Contacts.searchContactsByName("gagana");
+		System.out.println(number60);
+				
+		long number61 = Contacts.searchContactsByName("ganashree");
+		System.out.println(number61);
+
+		long number62 = Contacts.searchContactsByName("ganeshjewellery");
+		System.out.println(number62);
+
+		long number63 = Contacts.searchContactsByName("ganeshvinbnagar");
+		System.out.println(number63);
+
+		long number64 = Contacts.searchContactsByName("geetha");
+		System.out.println(number64);
+
+		long number65 = Contacts.searchContactsByName("geetha1");
+		System.out.println(number65);
+
+		long number66 = Contacts.searchContactsByName("girishbatru");
+		System.out.println(number66);
+
+		long number67 = Contacts.searchContactsByName("gourimca");
+		System.out.println(number67);
+
+		long number68 = Contacts.searchContactsByName("gowthammca");
+		System.out.println(number68);
+
+		long number69 = Contacts.searchContactsByName("gvm");
+		System.out.println(number69);
+
+		long number70 = Contacts.searchContactsByName("haleshsir");
+		System.out.println(number70);
+
+		long number71 = Contacts.searchContactsByName("haleshd");
+		System.out.println(number71);
+
+		long number72 = Contacts.searchContactsByName("harish");
+		System.out.println(number72);
+
+		long number73 = Contacts.searchContactsByName("harshini");
+		System.out.println(number73);
+
+		long number74 = Contacts.searchContactsByName("harshinibca");
+		System.out.println(number74);
+
+		long number75 = Contacts.searchContactsByName("harshitha");
+		System.out.println(number75);
+
+		long number76 = Contacts.searchContactsByName("heeba");
+		System.out.println(number76);
+
+		long number77 = Contacts.searchContactsByName("incharamca");
+		System.out.println(number77);
+
+		long number78 = Contacts.searchContactsByName("indhuja");
+		System.out.println(number78);
+
+		long number79 = Contacts.searchContactsByName("isiripg");
+		System.out.println(number79);
+
+		long number80 = Contacts.searchContactsByName("jaggaankal");
+		System.out.println(number80);
+
+		long number81 = Contacts.searchContactsByName("janyashree");
+		System.out.println(number81);
+
+		long number82 = Contacts.searchContactsByName("jnanavi");
+		System.out.println(number82);
+
+		long number83 = Contacts.searchContactsByName("joythika");
+		System.out.println(number83);
+
+		long number84 = Contacts.searchContactsByName("joythi1");
+		System.out.println(number84);
+
+		long number85 = Contacts.searchContactsByName("kalilsir");
+		System.out.println(number85);
+
+		long number86 = Contacts.searchContactsByName("kalyaniankal");
+		System.out.println(number86);
+
+		long number87 = Contacts.searchContactsByName("kamalaauntie");
+		System.out.println(number87);
+
+		long number88 = Contacts.searchContactsByName("kamala1");
+		System.out.println(number88);
+
+		long number89 = Contacts.searchContactsByName("kanakashree");
+		System.out.println(number89);
+
+		long number90 = Contacts.searchContactsByName("kavanaakka");
+		System.out.println(number90);
+
+		long number91 = Contacts.searchContactsByName("kavanasharath");
+		System.out.println(number91);
+
+		long number92 = Contacts.searchContactsByName("kavanaskg");
+		System.out.println(number92);
+
+		long number93 = Contacts.searchContactsByName("kavya");
+		System.out.println(number93);
+
+		long number94 = Contacts.searchContactsByName("kavyaakkabnglr");
+		System.out.println(number94);
+
+		long number95 = Contacts.searchContactsByName("kavyaanjali");
+		System.out.println(number95);
+
+		long number96 = Contacts.searchContactsByName("kavyabarki");
+		System.out.println(number96);
+
+		long number97 = Contacts.searchContactsByName("keerthana");
+		System.out.println(number97);
+
+		long number98 = Contacts.searchContactsByName("keerthanahyd");
+		System.out.println(number98);
+
+		long number99 = Contacts.searchContactsByName("keerthi");
+		System.out.println(number99);
+
+		long number100 = Contacts.searchContactsByName("krish");
+		System.out.println(number100);
+
+		long number101 = Contacts.searchContactsByName("kullamava");
+		System.out.println(number101);
+
+		long number102 = Contacts.searchContactsByName("lakshmitailor");
+		System.out.println(number102);
+
+		long number103 = Contacts.searchContactsByName("lathaantie");
+		System.out.println(number103);
+
+		long number104 = Contacts.searchContactsByName("lavanya");
+		System.out.println(number104);
+
+		long number105 = Contacts.searchContactsByName("leelavegetable");
+		System.out.println(number105);
+
+		long number106 = Contacts.searchContactsByName("leelavathi");
+		System.out.println(number106);
+
+		long number107 = Contacts.searchContactsByName("likitha");
+		System.out.println(number107);
+
+		long number108 = Contacts.searchContactsByName("lohithanna");
+		System.out.println(number108);
+
+		long number109 = Contacts.searchContactsByName("lokeshmava");
+		System.out.println(number109);
+
+		long number110 = Contacts.searchContactsByName("malikadhodamma");
+		System.out.println(number110);
+
+		long number111 = Contacts.searchContactsByName("manjulaantie");
+		System.out.println(number111);
+
+		long number112 = Contacts.searchContactsByName("manjunathasir");
+		System.out.println(number112);
+
+		long number113 = Contacts.searchContactsByName("manu");
+		System.out.println(number113);
+
+		long number114 = Contacts.searchContactsByName("manubro");
+		System.out.println(number114);
+
+		long number115 = Contacts.searchContactsByName("medhu");
+		System.out.println(number115);
+
+		long number116 = Contacts.searchContactsByName("modelingsmg");
+		System.out.println(number116);
+
+		long number117 = Contacts.searchContactsByName("modell");
+		System.out.println(number117);
+
+		long number118 = Contacts.searchContactsByName("modell1");
+		System.out.println(number118);
+
+		long number119 = Contacts.searchContactsByName("modell2");
+		System.out.println(number119);
+
+		long number120 = Contacts.searchContactsByName("monisha");
+		System.out.println(number120);
+
+		long number121 = Contacts.searchContactsByName("naliniakka");
+		System.out.println(number121);
+
+		long number122 = Contacts.searchContactsByName("namanackm");
+		System.out.println(number122);
+
+		long number123 = Contacts.searchContactsByName("nandhinipg");
+		System.out.println(number123);
+
+		long number124 = Contacts.searchContactsByName("nanjundhaankal");
+		System.out.println(number124);
+
+		long number125 = Contacts.searchContactsByName("nayana");
+		System.out.println(number125);
+
+		long number126 = Contacts.searchContactsByName("nayanamca");
+		System.out.println(number126);
+
+		long number127 = Contacts.searchContactsByName("neha");
+		System.out.println(number127);
+
+		long number128 = Contacts.searchContactsByName("nehaakkimca");
+		System.out.println(number128);
+
+		long number129 = Contacts.searchContactsByName("nehabca");
+		System.out.println(number129);
+	}
+}
+
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
